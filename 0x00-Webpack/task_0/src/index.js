fd129c6a95ca453949125f7fd7bad72b55635cd6
Mathias Owa Martins <owamathias@gmail.com>
2024-10-29 04:27:52 +0100

    Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,1 +1,7 @@
 import $ from "jquery";
+
+$("body").append(() => {
+    $("body").append("<p>Holberton Dashboard</p>");
+    $("body").append("<p>Dashboard data for the students");
+    $("body").append("<p>Copyright - Holberton School</p>");
+}
